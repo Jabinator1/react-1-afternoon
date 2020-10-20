634299aefa3a4e2199185077569eac4e1ab031e6
Jabinator1 <jabinator1@gmail.com>
2020-10-20 13:58:40 -0600

    finished step 1-3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+import FilterObject from './components/Topics/FilterObject';
+import EvenAndOdd from './components/Topics/EvenAndOdd';
+import Palindrome from './components/Topics/Palindrome';
+import Sum from './components/Topics/Sum';
 
 class App extends Component {
   render() {
-    
+    return (
+      <div>
+
+        <TopicBrowser />
+        <EvenAndOdd />
+        <FilterObject />
+        <Palindrome />
+        <Sum />
+      </div>
+    )
   }
 }
 
---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react"
+
+class EvenAndOdd extends Component {
+    constructor() {
+        super()
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: "",
+        }
+    }
+
+    inputChange = value => {
+        this.setState({
+            userInput: value
+        })
+    }
+
+    buttonClick = (userInput) => {
+        let arr = userInput.split(",")
+        const {evenArray, oddArray} = this.state
+
+        let even = []
+        let odd = []
+
+        for (let item of arr) {
+            console.log(item)
+            item % 2 === 0 ? even.push(item)
+            // this.setState({evenArray: [...evenArray, item]})
+            : odd.push(item)
+            // this.setState({oddArray: [...oddArray, item]})
+        }
+        this.setState({
+            evenArray: even,
+            oddArray: odd,
+        })
+
+    }
+
+    render() {
+        return (
+            <div className= "puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input onChange={e => this.inputChange(e.target.value)} className="inputLine"/>
+                <button onClick={() => {this.buttonClick(this.state.userInput)}} className="conformationButton">Split</button>
+                <span className="resultsBox">Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className="resultsBox">Odds: {JSON.stringify(this.state.oddArray)}</span>
+            </div>
+        )
+    }
+}
+
+export default EvenAndOdd 
